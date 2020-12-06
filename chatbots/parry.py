a49a4d64de95aeebb47b339a989173acf2e8122a
Rémi Felin <remi.felin@gmail.com>
2020-12-07 00:39:12 +0100

    Evolution parry et ajout img simulation

--- chatbots/parry.py
+++ chatbots/parry.py
@@ -9,10 +9,21 @@ import re
 user_template = "> "
 bot_template = "[BOT] {}"
 
-name = "Rémi"
+username = "Rémi"
 weather = ["cloudy", "rainy", "sunny", "windy", "gusty"]
 perception = []
 tolerance = 3
+stop_discussion = False
+talked = False
+
+LVL_COLERE_PEUR, LVL_MEFIANCE, LVL_AMOUR = 0, 0, 0
+MAX_PEUR_COLERE, MAX_MEFIANCE, MAX_AMOUR = 20, 15, 15
+
+key_angry = ["bookmaker", "money", "bet", "angry", "fulconis"]
+key_bully = ["bad", "hate", "low", "poor", "annoying", "kill"]
+key_compliment = ["good", "well", "nice", "pretty"]
+key_love = ["appareance", "family", "education"]
+key_afraid = ["mafia", "gun", "vendetta", "kill"]
 
 # Define a dictionary containing a list of responses for each message
 responses = {
@@ -44,23 +55,19 @@ def respond(message):
     time.sleep(1)
     # Nom
     name = find_name(message)
-    # Si on se répète sur des concepts
-    if perception.count(tolerance) != 0:
-        repeated_word = perception[perception.index(tolerance) - 1]
-        return responses["repeat"].format(repeated_word)
     if name is not None:
-        return "Hello, {0}!".format(name)
+        return bot_talk("Hello, {0}!".format(name))
     elif "weather" in message:
-        return random.choice(responses["weather"])
+        return bot_talk(random.choice(responses["weather"]))
     elif "name" in message and "?" in message:
-        return random.choice(responses["name"])
+        return bot_talk(random.choice(responses["name"]))
     elif "yes" == message.lower() or "no" == message.lower():
-        return "Why ?"
+        return bot_talk("Why ?")
     else:
         output = replace_pronouns(message)
         if "hello" in output:
-            return output
-        return output + " ?"
+            return bot_talk(output)
+        return bot_talk(output + "?")
 
 
 # Define replace_pronouns()
@@ -92,9 +99,15 @@ def replace_pronouns(message):
 def send_message(message):
     # print(user_template.format(message))
     update_perception(message)
-    response = respond(message)
-    print(bot_template.format(response))
-    time.sleep(0.75)
+    # Si on se répète sur des concepts
+    if perception.count(tolerance) != 0:
+        repeated_word = perception[perception.index(tolerance) - 1]
+        bot_talk(responses["repeat"].format(repeated_word))
+    # return false if discuss must be ended
+    # print(user_template.format(message))
+    update_states(message)
+    respond(message)
+    time.sleep(1)
 
 
 def update_perception(message):
@@ -107,16 +120,117 @@ def update_perception(message):
             perception.append(concept)
 
 
+def up_value(var, val):
+    var += val
+    return var
+
+
+def update_states(message):
+    message = message.lower()
+    # Will respond at your message
+    global LVL_MEFIANCE, LVL_COLERE_PEUR, LVL_AMOUR
+    # He's afraid about mafia, angry about bookmaker, like horseraces
+    strMsg = message.split()
+    for str in strMsg:
+        if str in key_angry:
+            LVL_COLERE_PEUR = up_value(LVL_COLERE_PEUR, 5)
+            rules_states()
+            for s in strMsg:
+                if s in key_bully:
+                    LVL_COLERE_PEUR = up_value(LVL_COLERE_PEUR, 10)
+                    rules_states()
+                    bot_talk("What do you said ???")
+            bot_talk("Don't talk about {}".format(str))
+        elif str in key_afraid:
+            LVL_MEFIANCE = up_value(LVL_MEFIANCE, 5)
+            rules_states()
+            for s in strMsg:
+                if s in key_bully:
+                    LVL_MEFIANCE = up_value(LVL_MEFIANCE, 10)
+                    rules_states()
+                    bot_talk("I didn't come here to suffer okay?")
+            bot_talk("{} afraid me ...".format(str))
+        elif str in key_bully:
+            LVL_COLERE_PEUR = up_value(LVL_COLERE_PEUR, 5)
+            rules_states()
+            for s in strMsg:
+                if s in key_love:
+                    LVL_COLERE_PEUR = up_value(LVL_COLERE_PEUR, 10)
+                    rules_states()
+                    bot_talk("{} ???".format(str))
+                elif s in key_angry:
+                    bot_talk("Don't talk about this...")
+            bot_talk("bruuh calm down ...")
+        elif str in key_love:
+            LVL_AMOUR = up_value(LVL_AMOUR, 5)
+            rules_states()
+            for s in strMsg:
+                if s in key_compliment:
+                    LVL_AMOUR = up_value(LVL_AMOUR, 10)
+                    rules_states()
+                    bot_talk("Wow you are too good !")
+            bot_talk("C'est bieng !")
+
+
+def log_states():
+    print("\n>>> peur/méfiance: {} - mefiance: {} - amour: {}\n".format(LVL_COLERE_PEUR, LVL_MEFIANCE, LVL_AMOUR))
+
+
+def rules_states():
+    # return false if discuss must be ended
+    global LVL_MEFIANCE, LVL_COLERE_PEUR, LVL_AMOUR, stop_discussion
+    # Rules for max range of different mood
+    if LVL_MEFIANCE >= MAX_MEFIANCE:
+        LVL_MEFIANCE = MAX_MEFIANCE
+        customize_responses()
+        stop_discussion = True
+    if LVL_COLERE_PEUR >= MAX_PEUR_COLERE:
+        LVL_COLERE_PEUR = MAX_PEUR_COLERE
+        customize_responses()
+        stop_discussion = True
+    if LVL_AMOUR >= MAX_AMOUR:
+        LVL_AMOUR = MAX_AMOUR
+        customize_responses()
+        stop_discussion = True
+
+
+def customize_responses():
+    global username
+    if username is None:
+        username = "Anonymous user"
+    # return a break (if false) or continue discuss (if true)
+    if LVL_AMOUR == MAX_AMOUR:
+        bot_talk("Wow... {}, would you being my friend ?".format(username))
+        response = input(user_template)
+        if response == "no":
+            bot_talk(":(")
+        elif response == "yes":
+            bot_talk("Cool !")
+        return True
+    if LVL_MEFIANCE == MAX_MEFIANCE:
+        bot_talk("You are too crazy {} ! bye !".format(username))
+        return False
+    if LVL_COLERE_PEUR == MAX_PEUR_COLERE:
+        bot_talk("{} ... You are so annoying !".format(username))
+        return False
+
+
+def bot_talk(msg):
+    global talked
+    if not talked:
+        print(bot_template.format(msg))
+        talked = True
+
+
 # SIMULATION
-sim = False
-while sim is False:
+while stop_discussion is False:
     msg = input(user_template)
     if msg == "exit":
-        print(bot_template.format("Goodbye !"))
-        sim = True
+        bot_talk("Goodbye !")
+        stop_discussion = True
     else:
         send_message(msg)
-
+        talked = False
 
 """
 import time
